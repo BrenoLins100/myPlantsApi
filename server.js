14f3a867563044dc0ce9d889322bf4753c82a8cd
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2023-07-26 19:18:48 -0300

    Alterando porta para 3333

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ fastify.register(cors,{
 })
 
 const start = async () =>{
-    const port = process.env.PORT || 3000
+    const port = process.env.PORT || 3333
     try{
         await fastify.listen({port: port})
     } catch(err){
